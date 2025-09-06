b0bbca70ab844d4a8742ec6d015fcaaea8217a43
lonons <vkolay01@mail.ru>
2025-09-06 11:00:00 +0700

    Edit Buttons

--- TG_Bot_HomeWork/Program.cs
+++ TG_Bot_HomeWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -19,9 +20,9 @@ namespace TG_HomeworkBot
                 while (end)
                 {
                     var str = Console.ReadLine();
-                    if (str != "exit") continue;
-                    end = false; break;
+                    if (str == "exit") end = false;
                 }
+                Console.WriteLine("Good luck!");
             }
             catch (Exception ex)
             {
@@ -48,23 +49,45 @@ namespace TG_HomeworkBot
 
             _bot.OnMessage += OnMessage;
             _bot.OnUpdate += OnUpdate;
+            _bot.OnError += OnError;
         }
 
         private async Task OnMessage(Message msg, UpdateType type)
         {
             if (msg.Text == "/start")
             {
-                await _bot.SendMessage(msg.Chat, "Welcome! Pick one direction",
-                    replyMarkup: new InlineKeyboardButton[] { "Left", "Right" });
+                // await _bot.SendMessage(msg.Chat, "Welcome! Pick one direction",
+                //     replyMarkup: new InlineKeyboardButton[] { "Left", "Right" });
+
+                var replyMarkup = new ReplyKeyboardMarkup()
+                {
+                    Keyboard = new[]
+                    {
+                        new[] { new KeyboardButton("Создать группу") },
+                        new[] { new KeyboardButton("Добавить ДЗ") },
+                        new[] { new KeyboardButton("Просмотреть ДЗ") },
+                        new[] { new KeyboardButton("Изменить ДЗ") },
+                    },
+                    ResizeKeyboard = true
+                };
+                
+                var sent = 
+                    await _bot.SendMessage(msg.Chat, "Who or Where are you?", replyMarkup: replyMarkup);
             }
         }
-        async Task OnUpdate(Update update)
+        private async Task OnUpdate(Update update)
         {
             if (update is { CallbackQuery: { } query }) // non-null CallbackQuery
             {
-                await _bot.AnswerCallbackQuery(query.Id, $"You picked {query.Data}");
-                await _bot.SendMessage(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
+               // await _bot.AnswerCallbackQuery(query.Id, $"You picked {query.Data}");
+               // await _bot.SendMessage(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
+               await _bot.SendMessage(query.Message!.Chat, "Removing keyboard", replyMarkup: new ReplyKeyboardRemove());
             }
+        }
+        
+        private async Task OnError(Exception exception, HandleErrorSource source)
+        {
+            Console.WriteLine(exception); // just dump the exception to the console
         }
     }
 }
